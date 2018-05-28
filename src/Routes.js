718dd1872c42edfa1316837c67d331dc3bcbffe4
Jess Poemape <jesspoemape@gmail.com>
2018-05-28 15:37:52 -0600

    routes set up; auth set up; pausing before session

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import AppliedRoute from './components/AppliedRoute';
+import Home from './containers/Home';
+import NotFound from'./containers/NotFound';
+import Login from './containers/Login';
+
+export default ({ childProps }) => 
+    <Switch>
+        <AppliedRoute path="/" exact component={Home} props={childProps} />
+        <AppliedRoute path="/login" component={Login} props={childProps} />
+        <Route component={NotFound} />
+    </Switch>;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "jpoemape-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://swnay7aodj.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_L29OJh3lA",
+    APP_CLIENT_ID: "18ejde2aacqvkuc2hs9hov7eki",
+    IDENTITY_POOL_ID: "us-east-2:a67c022e-779a-4172-9a8a-c6ae3268de31"
+  }
+};
